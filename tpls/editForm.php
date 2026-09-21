af52e5b2753a7c29fc939d697caed920ae2aec2e
Laszlo Toth <32448562+pachel@users.noreply.github.com>
2026-09-21 13:27:09 +0200

    sort url

--- tpls/editForm.php
+++ tpls/editForm.php
@@ -10,43 +10,55 @@ if(is_object($result)){
     $result = (array)$result;
 
 }
+
+
 ?>
 
 <form method="post">
     <?php foreach ($this->config["form"] AS $row): ?>
         <div class="row">
-            <?php foreach ($row AS $col): if ($col["type"] == "hidden"):
-                ?>
-                <input type="hidden" id="ta_form_<?= $col["name"] ?>" name="<?= $col["name"] ?>" value="<?= (isset($col["value"]) ? $col["value"] : (isset($result[$col["name"]]) ? $result[$col["name"]]:"")) ?>">
-            <?php else : ?>
-                <div class="col<?= (isset($col["bt_num"]) ? "-" . $col["bt_num"] : "") ?>">
-                    <div class="form-group mb-3">
-                        <label><?= $col["text"] ?></label>
-                        <?php if ($col["type"] == "textarea"): ?>
-                            <textarea class="form-control" id="ta_form_<?= $col["name"] ?>" placeholder="<?= $col["text"] ?>" name="<?= $col["name"] ?>"<?= (isset($col["required"]) && $col["required"]) ? " required=\"true\"" : "" ?>><?= (isset($col["value"]) ? $col["value"] : (isset($result[$col["name"]]) ? $result[$col["name"]] : "")) ?></textarea>
-                        <?php elseif ($col["type"] == "select"): ?>
-                            <select class="form-control" id="ta_form_<?= $col["name"] ?>" name="<?= $col["name"] ?>"<?= (isset($col["required"]) && $col["required"]) ? " required=\"true\"" : "" ?>>
-                                <?php if (isset($col["data"])): foreach ($col["data"] AS $option): ?>
-                                    <option value="<?= $option["value"] ?>"<?= (isset($result[$col["name"]]) ? ($result[$col["name"]] == $option["value"] ? " selected=\"true\"" : "") : (isset($option["default"]) && $option["default"] ? " selected=\"true\"" : "")) ?>><?= $option["text"] ?></option>
-                                <?php endforeach;
-                                endif; ?>
-                            </select>
-                        <?php elseif ($col["type"] == "select2"): ?>
-                            <select class="form-control select2" id="ta_form_<?= $col["name"] ?>" name="<?= $col["name"] ?>"<?= (isset($col["required"]) && $col["required"]) ? " required=\"true\"" : "" ?>>
-                                <option value="0">Válasszon</option>
-                                <?php if (isset($col["data"])): foreach ($col["data"] AS $option): ?>
-                                    <option value="<?= $option["value"] ?>"<?= (isset($result[$col["name"]]) ? ($result[$col["name"]] == $option["value"] ? " selected=\"true\"" : "") : (isset($option["default"]) && $option["default"] ? " selected=\"true\"" : "")) ?>><?= $option["text"] ?></option>
-                                <?php endforeach;
-                                endif; ?>
-                            </select>
-                        <?php elseif ($col["type"] == "hidden"):?>
-                            <input type="hidden" class="form-control" id="ta_form_<?= $col["name"] ?>" name="<?= $col["name"] ?>" value="<?= (isset($col["value"]) ? $col["value"] : (is_array($result) ? $result[$col["name"]] : $result->{$col["name"]})) ?>">
-                        <?php else: ?>
-                            <input type="<?= $col["type"] ?>" class="form-control" id="ta_form_<?= $col["name"] ?>" placeholder="<?= $col["text"] ?>" name="<?= $col["name"] ?>"<?= ((isset($col["required"]) && $col["required"]) ? " required=\"true\"" : "").(isset($col["maxlength"]) && is_numeric($col["maxlength"])?" maxlength=\"".$col["maxlength"]."\"":"") ?> value="<?= (isset($col["value"]) ? $col["value"] : $result[$col["name"]]) ?>">
-                        <?php endif; ?>
+            <?php foreach ($row AS $col):
+
+                if ($col["type"] == "hidden"):
+                    ?>
+                    <input type="hidden" id="ta_form_<?= $col["name"] ?>" name="<?= $col["name"] ?>" value="<?= (isset($col["value"]) ? $col["value"] : (isset($result[$col["name"]]) ? $result[$col["name"]]:"")) ?>">
+                <?php else : ?>
+                    <div class="col<?= (isset($col["bt_num"]) ? "-" . $col["bt_num"] : "") ?>">
+                        <div class="form-group mb-3">
+                            <label><?= $col["text"] ?></label>
+                            <?php if ($col["type"] == "textarea"): ?>
+                                <textarea class="form-control" id="ta_form_<?= $col["name"] ?>" placeholder="<?= $col["text"] ?>" name="<?= $col["name"] ?>"<?= (isset($col["required"]) && $col["required"]) ? " required=\"true\"" : "" ?>><?= (isset($col["value"]) ? $col["value"] : (isset($result[$col["name"]]) ? $result[$col["name"]] : "")) ?></textarea>
+                            <?php elseif ($col["type"] == "select"): ?>
+                                <select class="form-control" id="ta_form_<?= $col["name"] ?>" name="<?= $col["name"] ?>"<?= (isset($col["required"]) && $col["required"]) ? " required=\"true\"" : "" ?>>
+                                    <?php if (isset($col["data"])): foreach ($col["data"] AS $option):
+                                        if(is_object($option)){
+                                            $option = (array)$option;
+                                        }
+                                        ?>
+                                        <option value="<?= $option["value"] ?>"<?= (isset($result[$col["name"]]) ? ($result[$col["name"]] == $option["value"] ? " selected=\"true\"" : "") : (isset($option["default"]) && $option["default"] ? " selected=\"true\"" : "")) ?>><?= $option["text"] ?></option>
+                                    <?php endforeach;
+                                    endif; ?>
+                                </select>
+                            <?php elseif ($col["type"] == "select2"): ?>
+                                <select class="form-control select2" id="ta_form_<?= $col["name"] ?>" name="<?= $col["name"] ?>"<?= (isset($col["required"]) && $col["required"]) ? " required=\"true\"" : "" ?>>
+                                    <option value="0">Válasszon</option>
+                                    <?php if (isset($col["data"])): foreach ($col["data"] AS $option):
+                                        if(is_object($option)){
+                                            $option = (array)$option;
+                                        }
+                                        ?>
+                                        <option value="<?= $option["value"] ?>"<?= (isset($result[$col["name"]]) ? ($result[$col["name"]] == $option["value"] ? " selected=\"true\"" : "") : (isset($option["default"]) && $option["default"] ? " selected=\"true\"" : "")) ?>><?= $option["text"] ?></option>
+                                    <?php endforeach;
+                                    endif; ?>
+                                </select>
+                            <?php elseif ($col["type"] == "hidden"):?>
+                                <input type="hidden" class="form-control" id="ta_form_<?= $col["name"] ?>" name="<?= $col["name"] ?>" value="<?= (isset($col["value"]) ? $col["value"] : (is_array($result) ? $result[$col["name"]] : $result->{$col["name"]})) ?>">
+                            <?php else: ?>
+                                <input type="<?= $col["type"] ?>" class="form-control" id="ta_form_<?= $col["name"] ?>" placeholder="<?= $col["text"] ?>" name="<?= $col["name"] ?>"<?= ((isset($col["required"]) && $col["required"]) ? " required=\"true\"" : "").(isset($col["maxlength"]) && is_numeric($col["maxlength"])?" maxlength=\"".$col["maxlength"]."\"":"") ?> value="<?= (isset($col["value"]) ? $col["value"] : $result[$col["name"]]) ?>">
+                            <?php endif; ?>
+                        </div>
                     </div>
-                </div>
-            <?php endif;
+                <?php endif;
             endforeach; ?>
         </div>
     <?php endforeach; ?>
